eb8e2896fe1ce56ed4a3d7f749ad0e6a88e7133a
Scopecr <wjrcarrion@gmail.com>
2023-09-26 12:51:30 -0400

    fixed splice

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -3,6 +3,6 @@ word = "Holberton"
 word_first_3 = word
 word_last_2 = word
 middle_word = word
-print(f"First 3 letters: {word_first_3[3:0]}")
-print(f"Last 2 letters: {word_last_2[:-2]}")
+print(f"First 3 letters: {word_first_3[0:-6]}")
+print(f"Last 2 letters: {word_last_2[7:0]}")
 print(f"Middle word: {middle_word[1:-1]}")
