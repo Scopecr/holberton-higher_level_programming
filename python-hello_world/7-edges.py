92411348bbbb1d119018a72691f1acab0551905b
Scopecr <wjrcarrion@gmail.com>
2023-09-26 12:52:15 -0400

    fixing splice

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -4,5 +4,5 @@ word_first_3 = word
 word_last_2 = word
 middle_word = word
 print(f"First 3 letters: {word_first_3[0:-6]}")
-print(f"Last 2 letters: {word_last_2[7:0]}")
+print(f"Last 2 letters: {word_last_2[7:-9]}")
 print(f"Middle word: {middle_word[1:-1]}")
