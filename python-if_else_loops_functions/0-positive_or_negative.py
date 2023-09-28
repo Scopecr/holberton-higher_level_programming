d57df0e9e3a85c45615ea79bbdf3d1458ac6a416
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-09-28 11:07:03 -0400

    fixed bug where number 0 did not have space

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -4,6 +4,6 @@ number = random.randint(-10, 10)
 if number > 0:
     print("The number is positive: ", number)
 elif number == 0:
-    print("The number is 0", number)
+    print("The number is 0: ", number)
 elif number < 0:
     print("The number is negative: ", number)
