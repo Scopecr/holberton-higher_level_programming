6442ee8c7922f527bd066fb55a1aa5c77bc19124
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-09-28 11:11:10 -0400

    fixed bug where output was incorrect

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print("The number is positive:", number)
+    print("The number is greater than 0:", number)
 elif number == 0:
     print("The number is 0:", number)
 elif number < 0:
-    print("The number is negative:", number)
+    print("The number is less than 0:", number)
