ab0cd5f206e27b1ca6f99cb0ef9c5e818867e1c0
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-09-28 11:07:52 -0400

    fixed bug where space in number was too far off

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print("The number is positive: ", number)
+    print("The number is positive:", number)
 elif number == 0:
-    print("The number is 0: ", number)
+    print("The number is 0:", number)
 elif number < 0:
-    print("The number is negative: ", number)
+    print("The number is negative:", number)
