62cbe57efe91b29b745b4318a8cdfa6010c7a93c
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-09-29 07:52:05 -0400

    fixed pycodestyle error of lines having too much characters

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,11 +3,11 @@ import random
 number = random.randint(-10000, 10000)
 n = number
 last_d = abs(number) % 10
-if number < 0:
+if n < 0:
     last_d = -last_d
 if last_d > 5:
-    print('Last digit of {} is {} and is greater than 5'.format(number, last_d))
+    print('Last digit of {} is {} and is greater than 5'.format(n, last_d))
 elif last_d == 0:
-    print("Last digit of {} is {} and is 0".format(number, last_d))
+    print("Last digit of {} is {} and is 0".format(n, last_d))
 elif last_d < 6 != 0:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_d))
+    print("Last digit of {} is {} and is less than 6 and not 0".format(n, last_d))
