b40cc8065b49bd31745475c8c81fff7fe6fcf05a
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-09-29 13:48:10 -0400

    fixed bug preventing comma from stoping at 99

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    print('{:0=2d},'.format(i), end=" ")
+for i in range(0,100):
+    if i == 99:
+        print("{}".format(i))
+    else:
+        print('{:02}'.format(i), end=", ")
