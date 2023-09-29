a7490860f19143776371a4c016bb4a21f07beaa7
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-09-29 18:45:19 -0400

    fizbuzzfix

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz(n):
-    result = []
-    for i in range(1, 100):
+    for i in range(1, 101):
         if i % 3 == 0:
-            result.append("Fizz")
+            print("Fizz ", end="")
         elif i % 5 == 0:
-            result.append("Buzz")
+            print("Buzz ", end="")
         elif i % 3 == 0 and i % 5 == 0:
-            result.append("FizzBuzz")
-    return result
+            print("FizzBuzz ", end="")
+        else:
+            print("{:d} ".format(i), end="")
