e0d217caf8b449a487c064774552b9de54b34648
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-09-29 20:34:56 -0400

    executable

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
-def print_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d} argument.".format(n))
-        return
-    else:
-        if n == 1:
-            print("{:d} argument:".format(n))
-        else:
-            print("{:d} arguments:".format(n))
-        i = 1
-        while i <= n:
-            print("{:d}: {:s}".format(i, argv[i]))
-            i += 1
-
-
 if __name__ == "__main__":
     import sys
-    print_arg(sys.argv)
+argc = len(sys.argv)
+
+if argc == 1:
+    print("0 arguments.")
+elif argc == 2:
+    print("1 argument:")
+    print("1: {}".format(str(sys.argv[1])))
+else:
+    print("{} arguments:".format(argc - 1))
+    for i in range(1, argc):
+        print("{}: {}".format(i, str(sys.argv[i])))
