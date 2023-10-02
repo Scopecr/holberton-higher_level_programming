39d477b05d4f4eab809b80230f1c15547dce3181
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-10-02 19:00:33 -0400

    updated code

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for i in my_list:
-        if i <= 0:
-            return None
-        elif idx >= i:
-            return None
+    if idx > 0 and idx < len(my_list):
+        return my_list[idx]
+    else:
+        return None
