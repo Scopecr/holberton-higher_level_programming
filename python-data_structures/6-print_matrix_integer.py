3c5ad8692cba545f65020bfe96f4a1906dd70574
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-10-03 14:37:08 -0400

    Tate would be sad

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,9 @@
 #!/usn/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range (len(matrix)):
-        for j in range(len(i=[])):
-            print('{:d}'.format(i, j))
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j == len(matrix[i]) - 1:
+                print("{:d}".format(matrix[i][j], end=""))
+            else:
+                print("{:d} ".format(matrix[i][j]), end="")
+        print()
