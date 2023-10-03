33b1af50ad5de94f10b476752f246b7c26841863
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-10-03 09:43:28 -0400

    lets see

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     new_string = ""
     for chr in  my_string:
         if not (chr == "C" or chr == "c"):
-            new_string += chr
+            new_string.copy()
     return new_string
