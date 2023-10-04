6d0952c9f35dbc8a1c0ead9178fd0468975a4ae3
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-10-04 11:35:47 -0400

    10-divisable

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    result = my_list
+    for i in range(len(result)):
+        if (i % 2) == 0:
+            result [True]
+        else:
+            result[False] +=1
