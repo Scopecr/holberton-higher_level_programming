3db871598c02d8923c86a4aa9c6f10b31f52ff8e
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-10-04 18:41:13 -0400

    10 upload

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    result = my_list
-    for i in range(len(result)):
-        if (i % 2) == 0:
-            result [True]
-        else:
-            result[False] +=1
+    if my_list is not None:
+        new = list(my_list)
+        for i in range(len(my_list)):
+            if my_list[i] % 2 == 0:
+                new[i] = True
+            else:
+                new[i] = False
+        return new
