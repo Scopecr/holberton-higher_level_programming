4703a5432114dd76ff2b840f35956c00e7bd1ebf
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-10-05 07:56:57 -0400

    5 done

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ""
-    for chr in  my_string:
-        if not (chr == "C" or chr == "c"):
-            new_string += chr
-    return new_string
+    s = ""
+    copy = list(my_string)
+    for i in range(len(my_string)):
+        if my_string[i] == 'c':
+            copy.remove('c')
+        if my_string[i] == 'C':
+            copy.remove('C')
+    return s.join(copy)
