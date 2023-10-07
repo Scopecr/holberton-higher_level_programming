29642e6e03297348a63ad6c542eb3c1a93f72fae
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-10-06 21:03:24 -0400

    safe fix 0 task

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    i = 0
+def safe_print_integer(value):
     try:
-        while i < x:
-            print("{:d}".format(my_list[i]), end="")
-            i += i
+        print("{:d}".format(value))
+        return True
     except:
-        pass
-    print()
-    return i
+        return False
