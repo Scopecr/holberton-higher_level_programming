11b5c0328f72b37bd846517032b1a2b1a35b06d9
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-10-06 20:13:38 -0400

    Roman numbers updated

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,65 +1,13 @@
 #!/usr/bin/python3
-# Check Examples
-
-# python 3.7.1
-
-thousands = ["MMM", "MM", "M"]
-hundreds = ["CM", "DCCC", "DCC", "DC", "D",
-            "CD", "CCC", "CC", "C"]
-tens = ["XC", "LXXX", "LXX", "LX", "L",
-        "XL", "XXX", "XX", "X"]
-digits = ["IX", "VIII", "VII", "VI", "V",
-          "IV", "III", "II", "I"]
-
-
-def check_digit(string, nums):
-    for i in nums:
-        if i in string:
-            return list(reversed(nums)).index(i) + 1
-    return -1
-
-
-def convert_roman_to_int(string):
-    thousand = check_digit(string, thousands)
-    if thousand != -1:
-        delete = thousands[thousand - 1]
-        string = string.replace(delete, '')
-    else:
-        thousand = 0
-    hundred = check_digit(string, hundreds)
-    if hundred != -1:
-        delete = hundreds[hundred - 1]
-        string = string.replace(delete, '')
-    else:
-        hundred = 0
-    ten = check_digit(string, tens)
-    if ten != -1:
-        delete = tens[ten - 1]
-        string = string.replace(delete, '')
-    else:
-        ten = 0
-    digit = check_digit(string, digits)
-    if digit != -1:
-        delete = digits[digit - 1]
-        string = string.replace(delete, '')
-    else:
-        digit = 0
-    return thousand * 1000 + hundred * 100 + ten * 10 + digit
-
-
-def check_input(string):
-    for i in string:
-        if i not in 'MCDLXVI':
-            return False
-    return True
-
-
 def roman_to_int(roman_string):
-    if roman_string is None:
+    if type(roman_string) is not str or roman_string is None:
         return 0
-    if not isinstance(roman_string, str):
-        return 0
-    if not check_input(roman_string):
-        return 0
-    result = convert_roman_to_int(roman_string)
-    return result
+    nums = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    sum = 0
+    for i in range(len(roman_string)):
+        value = nums[roman_string[i]]
+        if i + 1 < len(roman_string) and nums[roman_string[i + 1]] > value:
+            sum -= value
+        else:
+            sum += value
+    return sum
