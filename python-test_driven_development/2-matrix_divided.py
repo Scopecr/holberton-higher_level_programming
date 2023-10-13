b86ef55a0a4683456fce208c846a924b7515936a
Scopecr <wjrcarrion@gmail.com>
2023-10-13 16:12:53 -0400

    removed comments

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
-    """Divide all elements of the matrix
-    Parameters:
-    matrix = matrix
-    div = integer dividing elements
-    
-    Local args:
-    new = list of lists
-    array = lists with new elements
-    """
-    if not isinstance(div, (int, float)):
-        raise TypeError('div must be a number')
-    if not div:
+    errorMessage = "matrix must be a matrix (list of lists) of integers/floats"
+    if not matrix:
+        raise TypeError(errorMessage)
+    if not isinstance(matrix, list):
+        raise TypeError(errorMessage)
+    for lists in matrix:
+        if not isinstance(lists, list):
+            raise TypeError(errorMessage)
+        for item in lists:
+            if not isinstance(item, int) and not isinstance(item, float):
+                raise TypeError(errorMessage)
+    for lists in matrix:
+        if len(lists) == 0:
+            raise TypeError(errorMessage)
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
+    if not all(len(lists) == len(matrix[0]) for lists in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+    if div == 0:
         raise ZeroDivisionError("division by zero")
-
-    new = []
-    array = []
-    for row in matrix:
-        if len(row) != len(matrix[0]):
-            raise TypeError('Each row of the matrix have the same size')
-        if not all(row) and all(matrix[row]) is False:
-            raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
-        for num in row:
-            if isinstance(num, (int, float)) is False:
-                raise TypeError('matrix must be a matrix (list of lists) of integer/floats')
-            array.append(round(num / div, 2))
-        new.append(array)
-        array = []
-    return new
+    return [[round(item / div, 2) for item in lists] for lists in matrix]
