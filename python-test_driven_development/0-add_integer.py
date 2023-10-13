a5bf10e0307a54090fc94fc5de862cf9c66f0f91
Scopecr <wjrcarrion@gmail.com>
2023-10-13 16:07:33 -0400

    fixed pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,10 +4,12 @@ Module to sum
 add_integer: adds two integers and return
 """
 def add_integer(a, b=98):
+
+
     """Return sum of a + b
     Args: a = int/float b = int/float
     """
-    
+
     if isinstance(a, (int, float)) is False:
         raise TypeError('a must be an integer')
     if isinstance(b, (int, float)) is False:
