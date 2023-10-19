31c4c9c8c265bad01c166842742ffa39cef68b42
Scopecr <wjrcarrion@gmail.com>
2023-10-19 15:26:08 -0400

    removed comments

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-"""
-Class that inherits form the lits class
-"""
 class MyList(list):
     pass
 
 def print_sorted(self):
-    """function that prints
-    the sorted list"""
     print(sorted(self))
