16b41042df3ea7cd388028f1d5924974d51913e4
Scopecr <wjrcarrion@gmail.com>
2023-10-20 17:52:16 -0400

    writing and creating file if it does not exist

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""function that writes text file utf-8"""
+
+
+def write_file(filename="", text=""):
+    with open(filename, mode="w", encoding="UTF-8") as f:
+        f.write(text)
