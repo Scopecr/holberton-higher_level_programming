91835f2476c4eec87c4b0cdee74ec3f285809309
Scopecr <wjrcarrion@gmail.com>
2023-10-20 17:56:36 -0400

    this cant be true

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,5 +3,7 @@
 
 
 def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF8) and returns the number of
+    characters written"""
     with open(filename, mode="w", encoding="UTF-8") as f:
         return f.write(text)
