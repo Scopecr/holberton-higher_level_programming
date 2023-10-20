8c7abbdaf34f76ce6ad5d4d9c333eb5c9c24e03c
Scopecr <wjrcarrion@gmail.com>
2023-10-20 17:31:23 -0400

    pycodestyle fixed

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,10 +2,12 @@
 """
 function that reads file that does not exist
 """
+
+
 def read_file(filename=""):
     """
     using with to open file: "" wit utf-8 encoding
     as file and print the read
     """
-    with open("", encoding="UTF-8" ) as file:
+    with open("", encoding="UTF-8") as file:
         print(file.read())
