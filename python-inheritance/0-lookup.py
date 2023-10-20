5bdf88a4ae6151030529b6fd368ea0605e7d59fa
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-10-20 08:27:09 -0400

    updated code and comments for the 10nth time

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Attribute lookup object and methods
-"""
+"""Function that returns the list of available attributes and methods
+of an object"""
+
+
 def lookup(obj):
-    """ Function that returns the list of object
-    """
+    """Function that returns the list of available attributes and methods
+    of an object"""
     return dir(obj)
