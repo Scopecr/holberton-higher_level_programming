b36527750c9c8e689c0399e0f7fc1ccd86e2456e
Scopecr <wjrcarrion@gmail.com>
2023-10-20 18:32:44 -0400

    fixed bug

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """Save an object into a JSON file."""
     with open(filename, mode="w", encoding="utf-8") as f:
-        f.write(my_obj, f)
+        json.dump(my_obj, f)
