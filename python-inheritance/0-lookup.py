b58aa204030abfed2650fcc56376362d5c28fce6
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-10-20 08:21:26 -0400

    fixed pycodestyle

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""
-Attribute lookup object and methods
+"""Attribute lookup object and methods
 """
 def lookup(obj):
-    """
-    Function that returns the list of object
+    """ Function that returns the list of object
     """
     return dir(obj)
