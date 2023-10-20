8f2922e684c8af4a49a01ee564ce2759f80f41cd
Scopecr <wjrcarrion@gmail.com>
2023-10-20 18:31:46 -0400

    5- save

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that writes obj to a text"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Save an object into a JSON file."""
+    with open(filename, mode="w", encoding="utf-8") as f:
+        f.write(my_obj, f)
