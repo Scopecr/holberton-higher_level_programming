f4307f4e07800c4ff4c4ab4355976faf58e4885a
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-10-23 19:48:23 -0400

    base and initpy file made

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Base Model
+"""
+
+
+class Base:
+    """
+    Base class for all models.
+    Attributes
+    """
+    __nb_objects = 0
+    
+    def __init__(self, id=None):
+        """
+        Init
+        Attributes:
+        id (): id
+        """
+        if id is not None:
+            self.id =id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    
