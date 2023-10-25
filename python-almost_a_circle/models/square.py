85e77b75ad4c26e250fb4619270831ddee0d06d1
Scopecr <wjrcarrion@gmail.com>
2023-10-25 19:24:49 -0400

    updated square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,61 +1,60 @@
 #!/usr/bin/python3
-"""
-the class square that inherits from Rectangle
-"""
-from .rectangle import Rectangle
+""" Square class """
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """
-        Class Square
-    """
+    """Square class inherits from Rectangle class"""
+    # constructor
+
     def __init__(self, size, x=0, y=0, id=None):
-        """
+        """ init method for square class
+        Args:
+            size (int): size of the square
+            x (int): x position of the square
+            y (int): y position of the square
+            id (int): id of the square
         """
         super().__init__(size, size, x, y, id)
-        self.size = size
-
-    @property
-    def size(self):
-        """
-            Size Getter
-        """
-        return self.width
-
-    @size.setter
-    def size(self, value):
-        """
-            Size Setter
-        """
-        self.height = value
-        self.width = value
 
     def __str__(self):
-        """informal string representation of the square"""
-        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x,
-                                                         self.y, self.width)
+        """ string representation of square """
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    # Getters
+    @property
+    def size(self):
+        """ size getter """
+        return self.width
+
+    # Setters
+    @size.setter
+    def size(self, value):
+        """ size setter
+        Args:
+            value (int): value to set size to"""
+        self.width = value
+        self.height = value
 
     def update(self, *args, **kwargs):
-        """update"""
-        i = 0
+        """update method for square class
+        Args:
+            args (list): list of arguments
+            kwargs (dict): dictionary of arguments
+        """
         if args:
-            for arg in args:
-                if i == 0:
-                    self.id = arg
-                if i == 1:
-                    self.size = arg
-                if i == 2:
-                    self.x = arg
-                if i == 3:
-                    self.y = arg
-                i += 1
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
         else:
-            for arg in kwargs:
-                setattr(self, arg, kwargs.get(arg))
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
     def to_dictionary(self):
-        """
-            returns the dictionary
-            representation of a square
-        """
+        """ dictionary representation of square """
         return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
