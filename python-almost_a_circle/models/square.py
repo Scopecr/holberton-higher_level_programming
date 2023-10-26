8e0e67a5721b1373fca13817470a9e72651e86bb
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-10-26 13:14:00 -0400

    squarepy

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,60 +1,67 @@
 #!/usr/bin/python3
-""" Square class """
+"""square class"""
+
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Square class inherits from Rectangle class"""
-    # constructor
+    """square class
+    Import from models/rectangle.py
+    Attributes:
+        size (int): size of square
+        x (int): x coordinate of square
+        y (int): y coordinate of square
+        id (int): id of square
+    Methods:
+        __init__(self, size, x=0, y=0, id=None)
+        size(self)              size(self, value)
+        area(self)
+        display(self)
+        __str__(self)
+        update(self, *args, **kwargs)
+        to_dictionary(self)"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """ init method for square class
-        Args:
-            size (int): size of the square
-            x (int): x position of the square
-            y (int): y position of the square
-            id (int): id of the square
+        """init method
+        Private instance attributes:
+            size (int): size of square
+            x (int): x coordinate of square
+            y (int): y coordinate of square
         """
         super().__init__(size, size, x, y, id)
+        self.size = size
 
-    def __str__(self):
-        """ string representation of square """
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
-
-    # Getters
     @property
     def size(self):
-        """ size getter """
+        """size getter method"""
         return self.width
 
-    # Setters
     @size.setter
     def size(self, value):
-        """ size setter
-        Args:
-            value (int): value to set size to"""
+        """size setter method"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.width = value
         self.height = value
 
+    def __str__(self):
+        """str method"""
+        return "[{}] ({}) {}/{} - {}".format(self.__class__.__name__, self.id,
+                                             self.x, self.y, self.size)
+
     def update(self, *args, **kwargs):
-        """update method for square class
-        Args:
-            args (list): list of arguments
-            kwargs (dict): dictionary of arguments
-        """
+        """update method"""
         if args:
-            if len(args) >= 1:
-                self.id = args[0]
-            if len(args) >= 2:
-                self.size = args[1]
-            if len(args) >= 3:
-                self.x = args[2]
-            if len(args) >= 4:
-                self.y = args[3]
+            attrs = ["id", "size", "x", "y"]
+            for i in range(len(args)):
+                setattr(self, attrs[i], args[i])
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
     def to_dictionary(self):
-        """ dictionary representation of square """
-        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
+        """to_dictionary method"""
+        attrs = ["id", "size", "x", "y"]
+        return {key: getattr(self, key) for key in attrs}
